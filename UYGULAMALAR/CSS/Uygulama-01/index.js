712a3f8e828c7c5f329be8e82c5f739f13a418eb
OrcunGokoluk <orcun.gokoluk@outlook.com>
2024-11-28 19:43:53 +0300

    updatev0.1

---
+++ UYGULAMALAR/CSS/Uygulama-01/index.js
@@ -0,0 +1,31 @@
+
+console.log("asd");
+const header=document.getElementById("headerEl") // headerEL
+
+header.textContent="JAVASCRIPT";
+header.style.backgroundColor="blue";
+header.style.color="white";
+header.style.borderRadius="1rem";
+
+
+
+const pageTitle=document.querySelector("#page_titleEl") // page_titleEl
+
+pageTitle.textContent="JAVASCRIPT";
+pageTitle.style.backgroundColor="green";
+pageTitle.style.color="white";
+pageTitle.style.borderRadius="1rem";
+
+const content=document.querySelector(".content") // .content
+
+content.textContent="JAVASCRIPT";
+content.style.backgroundColor="red";
+content.style.color="white";
+content.style.borderRadius="1rem";
+
+const footer=document.querySelector("footer") // footer
+
+footer.textContent="JAVASCRIPT";
+footer.style.backgroundColor="steelblue";
+footer.style.color="white";
+footer.style.borderRadius="1rem";
